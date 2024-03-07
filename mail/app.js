2b318e52e7f54dd8a54026795424d319411a1a76
EnricoClem <enrico.clem94@gmail.com>
2024-03-07 15:56:01 +0100

    ADD - console mail check

--- mail/app.js
+++ mail/app.js
@@ -3,18 +3,27 @@
 // stampa un messaggio appropriato sull’esito del controllo.
 
 // Creare una lista di email accettate
-let AcceptedEmailList = ['pippofranco@keficomail.com','rubryrubacuore@katya.it','gabibbo@belandi.mediaset','principenigeriano@help.ni']
+let acceptedEmailList = ['pippofranco@keficomail.com','rubryrubacuore@katya.it','gabibbo@belandi.mediaset','principenigeriano@help.ni'];
 
 // Input per il cliente di inserimento della propria mail
-const userEmail = document.getElementById('useremail')
+const userEmail = document.getElementById('useremail');
 
 // Click di invio della propria mail
-const buttonUserEmail = document.querySelector('[type="submit"]')
+const buttonUserEmail = document.querySelector('[type="submit"]');
 
 // Confornto della mail inserita con quelle accettate
 buttonUserEmail.addEventListener('click', function () {
-    console.log('avvio confronto')
+    console.log('avvio confronto');
 
+    for (let email = 0; email < acceptedEmailList.length; email++) {
+        const emailCheck = acceptedEmailList[email];
+
+        if (emailCheck === userEmail.value) {
+            console.log('Email accettata')
+        } else {
+            console.log('Email non registrata')
+        }
+    }
 })
 
 // SE la mail è presente nell'elenco fornire esito positivo
