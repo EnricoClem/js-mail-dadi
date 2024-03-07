e30ff350950e9451f4880346d8c6550f5849ec0e
EnricoClem <enrico.clem94@gmail.com>
2024-03-07 15:10:26 +0100

    ADD - pseudocode and html structure

--- mail/app.js
+++ mail/app.js
@@ -2,3 +2,9 @@
 // controlla che sia nella lista di chi può accedere,
 // stampa un messaggio appropriato sull’esito del controllo.
 
+// Creare una lista di email accettate
+// Input per il cliente di inserimento della propria mail
+// Click di invio della propria mail
+// Confornto della mail inserita con quelle accettate
+// SE la mail è presente nell'elenco fornire esito positivo
+// ALTRIMENTI forni re esito negativo
